425228f0c7fa92973c840210e1c1f4ac2e3d07f6
ginnupotter <58049566+ginnupotter@users.noreply.github.com>
2022-01-25 10:56:39 +0100

    Updates
    Added new flags and audios

--- flagScript.js
+++ flagScript.js
@@ -1,7 +1,10 @@
-var flagList1 = new Array("germany","netherlands","ireland","norway","uae","japan","indonesia","australia");
-var flagList2 = new Array("belgium","luxembourg","italy","iceland","jordan","bangladesh","poland","newzealand");
+var flagList1 = new Array("germany","netherlands","ireland","norway","uae","japan","indonesia","australia","finland","colombia","greece","india","singapore","botswana","russia");
+var flagList2 = new Array("belgium","luxembourg","italy","iceland","jordan","bangladesh","poland","newzealand","sweden","romania","uruguay","niger","turkey","argentina","slovenia");
 var quizIndex = new Array(flagList1.length);
 var counter = 0;
+var score = 0;
+var correctMsg = "Thats Correct :)";
+var incorrectMsg = "Sorry, thats not correct :(";
 
 function onPageLoad() {
       
@@ -13,22 +16,52 @@ function onPageLoad() {
 function quizNext()
 {
     setAudioImage();
-    var quesAudio = document.getElementById("questionAudio");
+
     var countryAudio1 = document.getElementById("countryAudio1");
     var countryAudio2 = document.getElementById("countryAudio2");
 
+    
     if(quizIndex[counter]==1) {
-        playAudio()
+        playAudio(countryAudio1);
+        document.getElementById("ques").innerHTML = "Which flag is for "+flagList1[counter].toUpperCase();
     } else if(quizIndex[counter]==2) {
-        document.getElementById("countryAudio2").play();
+        playAudio(countryAudio2);
+        document.getElementById("ques").innerHTML = "Which flag is for "+flagList2[counter].toUpperCase();
     }
 
+    document.getElementById("centerImg").style.visibility = "visible";
 
 }
 
+function checkAnswer(imgId) {
+
+    if(imgId==quizIndex[counter]) {
+        document.getElementById("ques").innerHTML = correctMsg;
+        document.getElementById("announce").src = "./sounds/correct.mp3";
+        document.getElementById("announce").play();
+        score++;
+    } else {
+        document.getElementById("ques").innerHTML = incorrectMsg;
+        document.getElementById("announce").src = "./sounds/incorrect.mp3";
+        document.getElementById("announce").play();
+    }
+
+    counter++; 
+    if(counter>=flagList1.length) {
+        document.getElementById("ques").innerHTML = "Your Score is: "+score;
+        document.getElementById("cueImg").className = "bigImage";
+        document.getElementById("cueImg").src = "./img/buttons/happySpideyDance.gif";
+        document.getElementById("cueImg").style.visibility = "visible";
+
+    } else {
+        document.getElementById("startButton").src = "./img/buttons/nextButton.png";
+        document.getElementById("startButton").style.visibility = "visible";
+    }
+    
+}
+
 function playAudio(audioTag) {
-
-
+    audioTag.play();
 }
 
 
@@ -45,6 +78,10 @@ function randomIntFromInterval(min, max) { // min and max included
 
   function setAudioImage() {
     
+    document.getElementById("cueImg").style.visibility = "hidden";
+    document.getElementById("startButton").style.visibility = "hidden";
+    
+    
     if (counter<flagList1.length) {
         setFlagImages(counter);
         setFlagAudio(counter);
@@ -56,8 +93,8 @@ function randomIntFromInterval(min, max) { // min and max included
   
 
   function setFlagImages(index) {
-    document.getElementById("img1").src="./img/"+flagList1[index]+".png";
-    document.getElementById("img2").src="./img/"+flagList2[index]+".png";
+    document.getElementById("1").src="./img/flags/"+flagList1[index]+".png";
+    document.getElementById("2").src="./img/flags/"+flagList2[index]+".png";
     
   }
 
